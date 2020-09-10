34ba11779210c346a29fabf87c6ac4b9160e4ff1
Nedra Krifa <nedrakrifa@gmail.com>
2020-09-10 22:09:33 +0200

    Updates

---
+++ precache-manifest.cd87623d35582fa283d5bb8f6d389098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e70866d03300b99867da7a1b6e39ff",
+    "url": "/Hooks-App/index.html"
+  },
+  {
+    "revision": "aff0048d0ddccd3b01fa",
+    "url": "/Hooks-App/static/css/main.7ac41d70.chunk.css"
+  },
+  {
+    "revision": "5b3064b55406baac1c52",
+    "url": "/Hooks-App/static/js/2.f560d60b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hooks-App/static/js/2.f560d60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff0048d0ddccd3b01fa",
+    "url": "/Hooks-App/static/js/main.ae9fe393.chunk.js"
+  },
+  {
+    "revision": "96d63311a4d63b17755f",
+    "url": "/Hooks-App/static/js/runtime-main.5ede990b.js"
+  }
+]);
